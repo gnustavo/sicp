e8dcb79f660484664c7ea3674a6bdf0e52ebc367
Gustavo L. de M. Chaves <gustavo@cpqd.com.br>
2016-01-15 23:25:00 -0200

    From 1.20 to 1.24

--- chapter-1.scm
+++ chapter-1.scm
@@ -188,4 +188,158 @@
                    q
                    (- count 1)))))
 
-;; 1.20 page 115
+;; 1.20
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+; 206 40 6 4 2 0
+
+(gcd 206 40)
+  (= 40 0)
+(gcd 40 (remainder 206 40))
+  (= (remainder 206 40) 0)
+  (= 6 0)
+(gcd 6 (remainder 40 6))
+  (= (remainder 40 6) 0)
+  (= 4 0)
+(gcd 4 (remainder 6 4))
+  (= (remainder 6 4) 0)
+  (= 2 0)
+(gcd 2 (remainder 4 2))
+  (= (remainder 4 2) 0)
+  (= 0 0)
+2
+
+;; 1.21
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n)
+         n)
+        ((divides? test-divisor n)
+         test-divisor)
+        (else (find-divisor
+               n
+               (+ test-divisor 1)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(smallest-divisor 199)
+; 199
+(smallest-divisor 1999)
+; 1999
+(smallest-divisor 19999)
+; 7
+
+;; 1.22
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime)
+                       start-time))))
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (search-for-primes begin end)
+  (if (even? begin)
+      (search-for-odd-primes (+ begin 1) end)
+      (search-for-odd-primes begin       end)))
+
+(define (search-for-odd-primes begin end)
+  (cond
+   ((< begin end)
+    (timed-prime-test begin)
+    (search-for-primes (+ begin 2) end))))
+
+(search-for-primes 1000 1020)
+(search-for-primes 10000 10039)
+(search-for-primes 100000 100045)
+(search-for-primes 1000000 1000039)
+(search-for-primes 10000000 10000100)
+(search-for-primes 10000000000 10000000100)
+(search-for-primes 100000000000 100000000050)
+; 1.15
+(search-for-primes 1000000000000 1000000000100)
+; 1.18
+(search-for-primes 10000000000000 10000000000100)
+; 3.66
+(search-for-primes 100000000000000 100000000000100)
+; 11.66
+
+;; 1.23
+
+(define (next n)
+  (if (= n 2)
+      3
+      (+ n 2)))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n)
+         n)
+        ((divides? test-divisor n)
+         test-divisor)
+        (else (find-divisor
+               n
+               (next test-divisor)))))
+
+(search-for-primes 100000000000 100000000050)
+; 1.15 -> 0.23
+(search-for-primes 1000000000000 1000000000100)
+; 1.18 -> 0.75
+(search-for-primes 10000000000000 10000000000100)
+; 3.66 -> 2.25
+(search-for-primes 100000000000000 100000000000100)
+; 11.66 -> 7.15
+
+;; 1.24
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder
+          (square (expmod base (/ exp 2) m))
+          m))
+        (else
+         (remainder
+          (* base (expmod base (- exp 1) m))
+          m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n)
+         (fast-prime? n (- times 1)))
+        (else false)))
+
+(define (start-prime-test n start-time)
+  (if (fast-prime? n 10)
+      (report-prime (- (runtime)
+                       start-time))))
+
+(search-for-primes 100000000000 100000000050)
+; 1.15 -> 0.23 -> 0.001
+(search-for-primes 1000000000000 1000000000100)
+; 1.18 -> 0.75 -> 0
+(search-for-primes 10000000000000 10000000000100)
+; 3.66 -> 2.25 -> 0
+(search-for-primes 100000000000000 100000000000100)
+; 11.66 -> 7.15 -> 0
+
+;; 1.25 page 125
+
